bc2bdb1f69c4b0f0b7e650f695265dbb9da1cc38
anujkulal <anujkulal333@gmail.com>
2025-07-22 19:16:57 +0530

    updated forms on 22-07-2025 19:16:57

---
+++ src/app/(main)/resume-editor/forms/PersonalInfoForm.tsx
@@ -0,0 +1,183 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { personalInfoSchema, personalInfoValues } from "@/lib/resumeSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+const PersonalInfoForm = () => {
+  const form = useForm<personalInfoValues>({
+    resolver: zodResolver(personalInfoSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      jobTitle: "",
+      city: "",
+      country: "",
+      phone: "",
+      email: "",
+    },
+  });
+
+//   useEffect(() => {
+//     const { unsubscribe } = form.watch(async () => {
+//       const isValid = await form.trigger();
+//       console.log("Form is valid:", isValid);
+//       if (!isValid) return;
+//     });
+//     return unsubscribe;
+//   }, [form]);
+
+
+// useEffect(() => {
+//   const subscription = form.watch((value, { name }) => {
+//     if (name === "photo") return; // Skip validation on photo
+
+//     const validate = async () => {
+//       const isValid = await form.trigger();
+//       console.log("Form is valid:", isValid);
+//     };
+
+//     validate();
+//   });
+
+//   return () => subscription.unsubscribe?.();
+// }, [form]);
+
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">Personal Information</h2>
+        <p className="text-muted-foreground text-sm">Tell us about yourself</p>
+      </div>
+      <Form {...form}>
+        <form action="" className="space-y-3">
+          <FormField
+            control={form.control}
+            name="photo"
+            render={({ field: { value, ...fieldValues } }) => (
+              <FormItem>
+                <FormLabel>Profile Photo</FormLabel>
+                <FormControl>
+                  <Input
+                    {...fieldValues}
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => {
+                      //File handling
+                      const file = e.target.files?.[0]; // select the single file from the **FileList object**, incase the user selects multiple files
+                      fieldValues.onChange(file);
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="grid grid-cols-2 gap-3">
+            <FormField
+            control={form.control}
+            name="firstName"
+            render={({field}) => (
+                <FormItem>
+                    <FormLabel>First Name</FormLabel>
+                    <FormControl>
+                        <Input {...field} placeholder="Anuj" />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="lastName"
+            render={({field}) => (
+                <FormItem>
+                    <FormLabel>Last Name</FormLabel>
+                    <FormControl>
+                        <Input {...field} placeholder="Kulal" />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="jobTitle"
+            render={({field}) => (
+                <FormItem>
+                    <FormLabel>Job Title</FormLabel>
+                    <FormControl>
+                        <Input {...field} placeholder="Software Engineer" />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="city"
+            render={({field}) => (
+                <FormItem>
+                    <FormLabel>City</FormLabel>
+                    <FormControl>
+                        <Input {...field} placeholder="Udupi" />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="country"
+            render={({field}) => (
+                <FormItem>
+                    <FormLabel>Country</FormLabel>
+                    <FormControl>
+                        <Input {...field} placeholder="India" />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="phone"
+            render={({field}) => (
+                <FormItem>
+                    <FormLabel>Phone</FormLabel>
+                    <FormControl>
+                        <Input type="tel" {...field} placeholder="1234567890" />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+            />
+            <FormField
+            control={form.control}
+            name="email"
+            render={({field}) => (
+                <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                        <Input type="email" {...field} placeholder="example@example.com" />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+            />
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default PersonalInfoForm;
