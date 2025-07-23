de337b2bfc7b11267b591580165105afb25c632d
anujkulal <anujkulal333@gmail.com>
2025-07-23 22:01:51 +0530

    updated Navbar.tsx on 23-07-2025 22:01:51

--- src/app/(main)/_components/Navbar.tsx
+++ src/app/(main)/_components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   const {theme} = useTheme();
   return (
     <header className="">
-      <div className="mx-auto flex max-w-7xl items-center justify-between p-3">
+      <div className="mx-auto flex max-w-7xl items-center justify-between p-2">
         <Link href={"/resumes"} className="flex items-center gap-2">
           <Image src={logo} alt="Logo" width={35} height={35} className="" />
           <span className="text-xl font-bold tracking-tight">ByteProfile</span>
