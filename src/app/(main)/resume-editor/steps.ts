a3f79c8c53b9c2eff6b8d2e772a5cc7011867a4c
anujkulal <anujkulal333@gmail.com>
2025-07-23 07:49:50 +0530

    updated steps.ts on 23-07-2025 07:49:50

--- src/app/(main)/resume-editor/steps.ts
+++ src/app/(main)/resume-editor/steps.ts
@@ -1,7 +1,12 @@
+import { ResumeEditorFormProps } from "@/lib/types";
 import GeneralInfoForm from "./forms/GeneralInfoForm";
 import PersonalInfoForm from "./forms/PersonalInfoForm";
 
-export const steps: {title: string; component: React.ComponentType; key: string}[] = [
-    {title: "General Info", component: GeneralInfoForm, key: "general-info"},
-    {title: "Personal Info", component: PersonalInfoForm, key: "personal-info"},
+export const steps: {
+  title: string;
+  component: React.ComponentType<ResumeEditorFormProps>;
+  key: string;
+}[] = [
+  { title: "General Info", component: GeneralInfoForm, key: "general-info" },
+  { title: "Personal Info", component: PersonalInfoForm, key: "personal-info" },
 ];
