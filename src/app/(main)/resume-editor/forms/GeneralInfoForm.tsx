79dcbdf960cf5ebe6589fc5f1d75cd12a849d7bb
anujkulal <anujkulal333@gmail.com>
2025-07-23 12:41:54 +0530

    updated GeneralInfoForm.tsx on 23-07-2025 12:41:54

--- src/app/(main)/resume-editor/forms/GeneralInfoForm.tsx
+++ src/app/(main)/resume-editor/forms/GeneralInfoForm.tsx
@@ -1,58 +1,80 @@
-import { generalInfoSchema, generalInfoValues } from '@/lib/resumeSchema'
-import React, { useEffect } from 'react'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { ResumeEditorFormProps } from '@/lib/types'
+import { generalInfoSchema, generalInfoValues } from "@/lib/resumeSchema";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { ResumeEditorFormProps } from "@/lib/types";
 
-const GeneralInfoForm = ({resumeData, setResumeData}: ResumeEditorFormProps) => {
+const GeneralInfoForm = ({
+  resumeData,
+  setResumeData,
+}: ResumeEditorFormProps) => {
   const form = useForm<generalInfoValues>({
     resolver: zodResolver(generalInfoSchema),
     defaultValues: {
       title: resumeData.title || "",
       description: resumeData.description || "",
-    }
-  })
+    },
+  });
 
   useEffect(() => {
     const subscription = form.watch((values) => {
-    setResumeData({...resumeData, ...values})
-  });
-
-  return () => subscription.unsubscribe();
-
+      setResumeData({ ...resumeData, ...values });
+    });
+    return () => subscription.unsubscribe();
   }, [form.watch, setResumeData]);
+
   return (
-    <div className='max-w-xl mx-auto space-y-8'>
+    <div className="mx-auto max-w-xl space-y-8">
       <div className="space-y-1.5 text-center">
-        <h2 className='text-2xl font-semibold'>General info</h2>
-        <p className="text-sm text-muted-foreground">This will not appear on your resume</p>
+        <h2 className="text-2xl font-semibold">General info</h2>
+        <p className="text-muted-foreground text-sm">
+          This will not appear on your resume
+        </p>
       </div>
       <Form {...form}>
-        <form className='space-y-3'>
-          <FormField control={form.control} name='title' render={({field}) => (
-            <FormItem>
-              <FormLabel>Project name</FormLabel>
-              <FormControl>
-                <Input {...field} placeholder='My cool resume' autoFocus/>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )} />
-          <FormField control={form.control} name='description' render={({field}) => (
-            <FormItem>
-              <FormLabel>Project description</FormLabel>
-              <FormControl>
-                <Input {...field} placeholder='A brief description of my resume'/>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )} />
+        <form className="space-y-3">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Project name</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="My cool resume" autoFocus />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Project description</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="A brief description of my resume"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </form>
       </Form>
     </div>
-  )
-}
+  );
+};
 
-export default GeneralInfoForm
+export default GeneralInfoForm;
