eebccd9f9936c895fef932c7dc46861f095381f6
anujkulal <anujkulal333@gmail.com>
2025-07-23 18:59:41 +0530

    updated steps.ts on 23-07-2025 18:59:41

--- src/app/(main)/resume-editor/steps.ts
+++ src/app/(main)/resume-editor/steps.ts
@@ -5,6 +5,9 @@ import ExperienceForm from "./forms/ExperienceForm";
 import EducationForm from "./forms/EducationForm";
 import SkillsForm from "./forms/SkillsForm";
 import SummaryForm from "./forms/SummaryForm";
+import HobbiesForm from "./forms/HobbiesForm";
+import AchievementsForm from "./forms/AchievementsForm";
+import ProjectsForm from "./forms/ProjectsForm";
 
 export const steps: {
   title: string;
@@ -13,8 +16,11 @@ export const steps: {
 }[] = [
   { title: "General Info", component: GeneralInfoForm, key: "general-info" },
   { title: "Personal Info", component: PersonalInfoForm, key: "personal-info" },
-  { title: "Experience Info", component: ExperienceForm, key: "experience-info" },
   { title: "Education Info", component: EducationForm, key: "education-info" },
+  { title: "Experience Info", component: ExperienceForm, key: "experience-info" },
+  { title: "Project Info", component: ProjectsForm, key: "project-info" },
   { title: "Skills Info", component: SkillsForm, key: "skills-info" },
+  { title: "Hobbies Info", component: HobbiesForm, key: "hobbies-info" },
+  { title: "Achievements Info", component: AchievementsForm, key: "achievements-info" },
   { title: "Summary Info", component: SummaryForm, key: "summary-info" },
 ];
