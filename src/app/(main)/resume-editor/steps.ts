6f5955022aa62c946038beb3de779c7363c16e8d
anujkulal <anujkulal333@gmail.com>
2025-07-23 22:01:51 +0530

    updated steps.ts on 23-07-2025 22:01:51

--- src/app/(main)/resume-editor/steps.ts
+++ src/app/(main)/resume-editor/steps.ts
@@ -8,19 +8,33 @@ import SummaryForm from "./forms/SummaryForm";
 import HobbiesForm from "./forms/HobbiesForm";
 import AchievementsForm from "./forms/AchievementsForm";
 import ProjectsForm from "./forms/ProjectsForm";
+import {
+  Info,
+  User,
+  GraduationCap,
+  Briefcase,
+  FolderKanban,
+  Brain,
+  Palette,
+  Trophy,
+  List
+} from "lucide-react";
+
+import type { LucideIcon } from "lucide-react";
 
 export const steps: {
   title: string;
+  icon?: LucideIcon; 
   component: React.ComponentType<ResumeEditorFormProps>;
   key: string;
 }[] = [
-  { title: "General Info", component: GeneralInfoForm, key: "general-info" },
-  { title: "Personal Info", component: PersonalInfoForm, key: "personal-info" },
-  { title: "Education Info", component: EducationForm, key: "education-info" },
-  { title: "Experience Info", component: ExperienceForm, key: "experience-info" },
-  { title: "Project Info", component: ProjectsForm, key: "project-info" },
-  { title: "Skills Info", component: SkillsForm, key: "skills-info" },
-  { title: "Hobbies Info", component: HobbiesForm, key: "hobbies-info" },
-  { title: "Achievements Info", component: AchievementsForm, key: "achievements-info" },
-  { title: "Summary Info", component: SummaryForm, key: "summary-info" },
+  { title: "General Info", icon: Info, component: GeneralInfoForm, key: "general-info" },
+  { title: "Personal Info", icon: User, component: PersonalInfoForm, key: "personal-info" },
+  { title: "Education Info",icon: GraduationCap, component: EducationForm, key: "education-info" },
+  { title: "Experience Info", icon: Briefcase, component: ExperienceForm, key: "experience-info" },
+  { title: "Project Info", icon: FolderKanban, component: ProjectsForm, key: "project-info" },
+  { title: "Skills Info", icon: Brain, component: SkillsForm, key: "skills-info" },
+  { title: "Hobbies Info", icon: Palette, component: HobbiesForm, key: "hobbies-info" },
+  { title: "Achievements Info", icon: Trophy, component: AchievementsForm, key: "achievements-info" },
+  { title: "Summary Info", icon: List, component: SummaryForm, key: "summary-info" },
 ];
