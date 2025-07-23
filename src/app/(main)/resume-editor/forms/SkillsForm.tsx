86d7bcbfad4ed6d6480350f28a48748049d47030
anujkulal <anujkulal333@gmail.com>
2025-07-23 13:20:13 +0530

    updated SkillsForm.tsx on 23-07-2025 13:20:13

--- src/app/(main)/resume-editor/forms/SkillsForm.tsx
+++ src/app/(main)/resume-editor/forms/SkillsForm.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { SkillsSchema, SkillsValues } from "@/lib/resumeSchema";
+import { ResumeEditorFormProps } from "@/lib/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { useEffect } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
 
-const SkillsForm = () => {
+const SkillsForm = ({ resumeData, setResumeData }: ResumeEditorFormProps) => {
+  const form = useForm<SkillsValues>({
+    resolver: zodResolver(SkillsSchema),
+    defaultValues: {
+      skills: resumeData.skills || [],
+    },
+  });
+
+  useEffect(() => {
+    const subscription = form.watch((values) => {
+      setResumeData({
+        ...resumeData,
+        skills:
+          values.skills
+            ?.filter((skill) => skill != undefined)
+            .map((skill) => skill.trim())
+            .filter((skill) => skill !== "") || [],
+      });
+    });
+    return () => subscription.unsubscribe();
+  }, [form.watch, setResumeData]);
+
   return (
-    <div>SkillsForm</div>
-  )
-}
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">Skills Info</h2>
+        <p className="text-muted-foreground text-sm">Add your skills</p>
+      </div>
+      <Form {...form}>
+        <form className="space-y-3">
+          <FormField
+            control={form.control}
+            name={`skills`}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Skills</FormLabel>
+                <FormControl>
+                  <Textarea
+                    {...field}
+                    placeholder="JavaScript, TypeScript, React.js Next.js ..."
+                    onChange={(e) => {
+                        const skills = e.target.value.split(",");
+                        field.onChange(skills);
+                    }}
+                    autoFocus
+                  />
+                </FormControl>
+                <FormDescription>List your skills separated by commas</FormDescription>
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
+};
 
-export default SkillsForm
+export default SkillsForm;
