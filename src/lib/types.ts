5231d4eff41746a96fa347d860f1ffb379164870
anujkulal <anujkulal333@gmail.com>
2025-07-26 22:59:48 +0530

    updated types.ts on 26-07-2025 22:59:48

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,18 @@
+import { Prisma } from "@/generated/prisma";
 import { ResumeValues } from "./resumeSchema";
 
 export interface ResumeEditorFormProps {
     resumeData: ResumeValues;
     setResumeData: (data: ResumeValues) => void;
 }
+
+
+export const resumeDataInclude = {
+  experiences: true,
+  educations: true,
+  projects: true,
+} satisfies Prisma.ResumeInclude;
+
+export type ResumeServerData = Prisma.ResumeGetPayload<{
+  include: typeof resumeDataInclude;
+}>;
