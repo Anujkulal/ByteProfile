fdcf92f33254e199c05efcae67603ba18fa0c27f
anujkulal <anujkulal333@gmail.com>
2025-07-26 11:38:07 +0530

    updated ResumePreviewSection.tsx on 26-07-2025 11:38:06

--- src/app/(main)/resume-editor/ResumePreviewSection.tsx
+++ src/app/(main)/resume-editor/ResumePreviewSection.tsx
@@ -1,32 +1,53 @@
 import ColorPicker from "@/components/home/ColorPicker";
-import ResumePreviewer from "@/components/home/ResumePreviewer";
 import { ResumeValues } from "@/lib/resumeSchema";
 import { cn } from "@/lib/utils";
 import React from "react";
+import { templates } from "./templates";
 
 interface ResumePreviewSectionProps {
   resumeData: ResumeValues;
   setResumeData: (data: ResumeValues) => void;
+  selectedTemplate: string;
   className?: string;
+  printRef?: React.RefObject<HTMLDivElement | null>; // Optional ref for printing
 }
 
 const ResumePreviewSection = ({
   resumeData,
   setResumeData,
+  selectedTemplate,
   className,
+  printRef,
 }: ResumePreviewSectionProps) => {
-  return (
-    <div className={cn(
-      "hidden w-full md:flex md:w-1/2", 
-      "flex-col overflow-hidden",
-      className
-    )}>
-      <div className="flex-1 overflow-y-auto bg-slate-50 p-4 dark:bg-slate-900/50">
-        <div className="flex h-full w-full items-start justify-center">
-          <ResumePreviewer 
+  const selectedTemplateObj = templates.find(
+    (template) => template.key === selectedTemplate,
+  );
+  const TemplateComponent =
+    selectedTemplateObj?.component || templates[0].component; // Fallback to first template
+
+return (
+    <div
+      className={cn(
+        "hidden w-full md:flex md:w-1/2",
+        "flex-col overflow-hidden",
+        className,
+      )}
+    >
+      <div className="flex-1 overflow-y-auto p-4 dark:bg-slate-900/50">
+        <div className="flex h-full w-full items-start justify-center" >
+          {/* <ResumePreviewer2
             resumeData={resumeData} 
-            className="w-full max-w-[600px] shadow-lg border border-slate-200 dark:border-slate-700"
-          />
+            className={cn("w-full max-w-[600px] shadow-lg rounded-2xl border-8 border-slate-900 dark:border-slate-700")}
+          /> */}
+          {/* <div className="print:shadow-none print:border-none" ref={printRef}> */}
+            <TemplateComponent
+              resumeData={resumeData}
+              className={cn(
+                "w-full max-w-[600px] rounded-2xl border-8 border-slate-900 shadow-lg dark:border-slate-700 print:border-none print:shadow-none",
+              )}
+              printRef={printRef} // Pass the ref to template component
+            />
+          {/* </div> */}
         </div>
       </div>
     </div>
