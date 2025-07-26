2200ef3cf4253c370fc89149b4d19532b2205e24
anujkulal <anujkulal333@gmail.com>
2025-07-26 22:59:48 +0530

    updated page.tsx on 26-07-2025 22:59:48

--- src/app/(main)/resume-editor/page.tsx
+++ src/app/(main)/resume-editor/page.tsx
@@ -1,14 +1,36 @@
 import { Metadata } from 'next'
 import React from 'react'
 import ResumeEditorClient from './ResumeEditorClient'
+import { auth } from '@clerk/nextjs/server';
+import prisma from '@/lib/prisma';
+import { resumeDataInclude } from '@/lib/types';
 
 export const metadata: Metadata = {
     title: 'Resume Editor',
 }
 
-const ResumeEditorPage = () => {
+interface PageProps {
+  searchParams: Promise<{ resumeId?: string }>;
+}
+
+const ResumeEditorPage = async ({ searchParams }: PageProps) => {
+  const { resumeId } = await searchParams;
+
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const resumeToEdit = resumeId
+    ? await prisma.resume.findUnique({
+        where: { id: resumeId, userId },
+        include: resumeDataInclude,
+      })
+    : null;
+
   return (
-    <ResumeEditorClient />
+    <ResumeEditorClient resumeToEdit={resumeToEdit} />
   )
 }
 
