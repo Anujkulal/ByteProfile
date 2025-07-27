481098a2a82f75e939440a4cd2476528c240154a
anujkulal <anujkulal333@gmail.com>
2025-07-27 16:45:03 +0530

    updated PersonalInfoForm.tsx on 27-07-2025 16:45:03

--- src/app/(main)/resume-editor/forms/PersonalInfoForm.tsx
+++ src/app/(main)/resume-editor/forms/PersonalInfoForm.tsx
@@ -56,7 +56,6 @@ const PersonalInfoForm = ({ resumeData, setResumeData }: ResumeEditorFormProps) 
     <div className="mx-auto max-w-xl space-y-6">
       <div className="space-y-1.5 text-center">
         <h2 className="text-2xl font-semibold">Personal Information</h2>
-        <p className="text-muted-foreground text-sm">Tell us about yourself</p>
       </div>
       <Form {...form}>
         <form action="" className="space-y-3">
